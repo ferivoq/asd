da2ff2cae0f5c09a2f466243c0e7301cfa8b4484
FeriVOQ <36544651+ferivoq@users.noreply.github.com>
2024-03-27 20:38:41 +0100

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,7 +1,7 @@
 // ==MiruExtension==
 // @name         AnimeDrive
 // @version      v1.0.0
-// @author       FeriVOQ, Hun0r
+// @author       FeriVOQ
 // @lang         hu
 // @license      MIT
 // @icon         https://cdn.discordapp.com/icons/702098578251841577/be50b5dbcb30bd99512b6df29284db0c.webp?size=96
@@ -26,6 +26,13 @@ export default class extends Extension {
       type: "input",
       description: "API URL",
       defaultValue: "http://192.168.0.185:5000",
+    });
+    this.registerSetting({
+      title: "Preferred quality",
+      key: "prefQuality",
+      type: "input",
+      description: "Choose between 360p/480p/720p/1080p",
+      defaultValue: "480p",
     });
   }
 
@@ -68,11 +75,20 @@ export default class extends Extension {
   }
 
   async watch(url) {
+    const quality = await this.getSetting("prefQuality");
     const res = await this.req(url);
+    const prefQuality = res.sources.find(source => source.quality === quality);
 
-    return {
-      type: "mp4",
-      url: res.sources.multi.url,
-    };
+    if (prefQuality) {
+      return {
+        type: "mp4",
+        url: prefQuality.url,
+      };
+    } else {
+      return {
+        type: "mp4",
+        url: res.sources.pop().url,
+      };
+    }
   }
 }
